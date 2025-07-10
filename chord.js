42cdd237b850aaddc4b4cc18d67fd080669c8110
Aleqzander Baker <aleqzanderbaker@gmail.com>
2025-07-10 08:49:20 -0600

    added major seventh chords

--- chord.js
+++ chord.js
@@ -21,7 +21,7 @@ const Bmaj = new Chord("Bmaj", document.getElementById('Bmaj'));
 
 
 
-majors = [Cmaj, Csmaj, Dmaj, Dsmaj, Emaj, Fmaj, Fsmaj, Gmaj, Gsmaj, Amaj, Asmaj, Bmaj];
+const majors = [Cmaj, Csmaj, Dmaj, Dsmaj, Emaj, Fmaj, Fsmaj, Gmaj, Gsmaj, Amaj, Asmaj, Bmaj];
 
 
 //////////////minors///////////////////
@@ -38,7 +38,25 @@ const Amin = new Chord("Amin", document.getElementById('Amin'));
 const Asmin = new Chord("A#min", document.getElementById('Asmin'));
 const Bmin = new Chord("Bmin", document.getElementById('Bmin'));
 
-minors = [Cmin, Csmin, Dmin, Dsmin, Emin, Fmin, Fsmin, Gmin, Gsmin, Amin, Asmin, Bmin];
+const minors = [Cmin, Csmin, Dmin, Dsmin, Emin, Fmin, Fsmin, Gmin, Gsmin, Amin, Asmin, Bmin];
+
+
+/////////////major sevenths////////////////////
+const Cmaj7 = new Chord("Cmaj7", document.getElementById('Cmaj7'));
+const Csmaj7 = new Chord("C#maj7", document.getElementById('Csmaj7'));
+const Dmaj7 = new Chord("Dmaj7", document.getElementById('Dmaj7'));
+const Dsmaj7 = new Chord("D#maj7", document.getElementById('Dsmaj7'));
+const Emaj7 = new Chord("Emaj7", document.getElementById('Emaj7'));
+const Fmaj7 = new Chord("Fmaj7", document.getElementById('Fmaj7'));
+const Fsmaj7 = new Chord("F#maj7", document.getElementById('Fsmaj7'));
+const Gmaj7 = new Chord("Gmaj7", document.getElementById('Gmaj7'));
+const Gsmaj7 = new Chord("G#maj7", document.getElementById('Gsmaj7'));
+const Amaj7 = new Chord("Amaj7", document.getElementById('Amaj7'));
+const Asmaj7 = new Chord("A#maj7", document.getElementById('Asmaj7'));
+const Bmaj7 = new Chord("Bmaj7", document.getElementById('Bmaj7'));
+
+
+const majorSevenths = [Cmaj7, Csmaj7, Dmaj7, Dsmaj7, Emaj7, Fmaj7, Fsmaj7, Gmaj7, Gsmaj7, Amaj7, Asmaj7, Bmaj7];
 
 
 function playRandomMajor(){
@@ -49,10 +67,14 @@ function playRandomMinor(){
     minors[Math.floor(Math.random() * 11)].audio.play();
 }
 
-chords=[majors, minors];
+function playRandomMajorSeventh(){
+    majorSevenths[Math.floor(Math.random() * 11)].audio.play();
+}
+
+const chords=[majors, minors, majorSevenths];
 
 function playRandomChord(){
-    chords[Math.floor(Math.random()* 2)][Math.floor(Math.random() * 11)].audio.play();
+    chords[Math.floor(Math.random()* chords.length)][Math.floor(Math.random() * 11)].audio.play();
 }
 
 
@@ -60,9 +82,7 @@ userInput = document.getElementById("search-input");
 output = document.getElementById("output");
 
 userInput.addEventListener("input", () => {
-    // output.textContent = userInput.value;
-    // output.textContent = allchords[1];
-    allchords = [];
+    let allchords = [];
     for(i = 0; i < chords.length; i++){
         allchords = allchords.concat(chords[i]);
     }
